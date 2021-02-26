f3e09f32a360ea835522e7444cac30eb0364acdb
GeneTv <contact@genetv.net>
2021-02-26 15:41:27 +0100

    Implemented UI elements

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,14 +4,44 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+    public void start(Stage primaryStage) throws Exception {
+        // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        GridPane root = new GridPane();
+
+        /*
+         * TODO:
+         *  - Alignment, Padding, Gaps und Fonts setzen
+         */
+
+        // UI Controls
+        Text loginTxt = new Text("Login:");
+        Label lblUsr = new Label("Username:");
+        TextField txtUsr = new TextField();
+        Label lblPwd = new Label("Password");
+        PasswordField txtPwd = new PasswordField();
+        Button loginBtn = new Button("Login");
+
+        // add UI Controls to Grid
+        root.add(loginTxt, 0, 0, 2, 1);
+        root.add(lblUsr, 0, 1);
+        root.add(txtUsr, 1, 1);
+        root.add(lblPwd, 0, 2);
+        root.add(txtPwd, 1, 2);
+        root.add(loginBtn, 1, 3);
+
+        // set Scene and show Stage
+        primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
