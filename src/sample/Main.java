ece959c613a933ef177003ca02a075b9cfeb6a9a
GeneTv <contact@genetv.net>
2021-03-05 14:54:32 +0100

    Implemented xml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,111 +2,27 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import sample.util.HTTPHandler;
-
-import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        GridPane root = new GridPane();
-        root.setPadding(new Insets(25));
-        root.setAlignment(Pos.CENTER);
-        root.setHgap(10);
-        root.setVgap(10);
-
-        // UI Controls
-        Text loginTxt = new Text("Login");
-        loginTxt.setFont(Font.font("Tahoma", FontWeight.NORMAL, 30));
-        loginTxt.setFill(Color.rgb(36, 48, 94));
-
-        Label lblUsr = new Label("Username:");
-        TextField txtUsr = new TextField();
-
-        Label lblPwd = new Label("Password");
-        PasswordField txtPwd = new PasswordField();
-
-        Text lblErr = new Text("");
-        lblErr.setFill(Color.rgb(247, 92, 108));
-
-        Button loginBtn = new Button("Login");
-
-        /*
-         * TODO:
-         *  - Alignment, Padding, Gaps und Fonts setzen
-         */
-
-
-        // add UI Controls to Grid
-        root.add(loginTxt, 0, 0, 2, 1);
-        root.add(lblUsr, 0, 1);
-        root.add(txtUsr, 1, 1);
-        root.add(lblPwd, 0, 2);
-        root.add(txtPwd, 1, 2);
-        root.add(lblErr, 1, 3);
-        root.add(loginBtn, 1, 4);
-
-        loginBtn.setOnAction(event -> {
-            String username = txtUsr.getText();
-            String password = txtPwd.getText();
-
-            // Init errors
-            ArrayList<String> errors = new ArrayList<>();
-            lblErr.setText("");
-
-
-            // TODO: Create validator class to make validation easier.
-            if(username.length() < 1)
-                errors.add("Please provide a username.");
-
-            if(password.length() < 4)
-                errors.add("Please provide a password with at least 4 characters.");
-
-            if(errors.size() != 0) {
-                // Show errors and prevent outgoing request.
-                lblErr.setText(String.join("\n", errors));
-                return;
-            }
-            try {
-                switch (HTTPHandler.post("/login", "{ \"username\": \"" + txtUsr.getText() + "\", \"password\": \"" + txtPwd.getText() + "\"}")) {
-                    case "200":
-                        System.out.println("Logged in!");
-                        System.exit(0);
-                        break;
-                    case "401":
-                        lblErr.setText("Password is wrong.");
-                        break;
-                    default:
-                        lblErr.setText("Password is wrong.");
-                        //lblErr.setText("Unknown error occurred. Try it again.");
-                }
-
-            } catch (IOException e) {
-                System.out.println(e);
-                lblErr.setText("Unknown error occurred. Try it again.");
-            }
-        });
+        Parent root = FXMLLoader.load(getClass().getResource("assets/sample.fxml"));
 
         // set Scene and show Stage
         primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(root, 500, 415));
+        //primaryStage.getClass().getClassLoader().getResource("assets/style.css").toExternalForm();
         primaryStage.show();
     }
 
--- src/sample/controller/LoginController.java
+++ src/sample/controller/LoginController.java
@@ -2,19 +2,64 @@ package sample.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import sample.util.HTTPHandler;
 
-import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class LoginController {
 
     @FXML
-    private TextField usernameTextField;
+    private TextField usernameField;
 
     @FXML
-    private TextField passwordTextField;
+    private PasswordField passwordField;
+
+    @FXML
+    private Text actiontarget;
 
 
-    public void onClick(ActionEvent event) {
-        System.out.println("Test");
+    public void signIn(ActionEvent event) {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        // Init errors
+        ArrayList<String> errors = new ArrayList<>();
+        actiontarget.setText("");
+
+
+        // TODO: Create validator class to make validation easier.
+        if(username.length() < 1)
+            errors.add("Please provide a username.");
+
+        if(password.length() < 4)
+            errors.add("Please provide a password with at least 4 characters.");
+
+        if(errors.size() != 0) {
+            // Show errors and prevent outgoing request.
+            actiontarget.setText(String.join("\n", errors));
+            return;
+        }
+        try {
+            switch (HTTPHandler.post("/login", "{ \"username\": \"" + username + "\", \"password\": \"" + password + "\"}")) {
+                case "200":
+                    System.out.println("Logged in!");
+                    System.exit(0);
+                    break;
+                case "401":
+                    actiontarget.setText("Password is wrong.");
+                    break;
+                default:
+                    actiontarget.setText("Password is wrong.");
+                    //actiontarget.setText("Unknown error occurred. Try it again.");
+            }
+
+        } catch (IOException e) {
+            System.out.println(e);
+            actiontarget.setText("Unknown error occurred. Try it again.");
+        }
     }
 }
